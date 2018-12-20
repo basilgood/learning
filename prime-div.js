261b668b44043fcfa08e5526ba3a4ce7aa854a01
vasile <elsile69@yahoo.com>
2018-12-20 16:10:19 +0200

    ok trick

--- prime-div.js
+++ prime-div.js
@@ -2,8 +2,12 @@ function divisors(num) {
   let array = [];
 
   for (let j = 2; j <= num; j++) {
+    let ok = false;
     while (num % j === 0) {
       num = num / j;
+      ok = true;
+    }
+    if (ok) {
       array[array.length] = j;
     }
   }
@@ -14,10 +18,18 @@ function divisors(num) {
 
 function primeSort(arr) {
   let arrRes = [];
+  let lastArr = [];
   for (let i = 0; i < arr.length; i++) {
-    arrRes[arrRes.length] = (divisors(arr[i]));
+    let divs = (divisors(arr[i]));
+    arrRes[arrRes.length] = divs[i];
+    // // console.log(divs);
+    // for (let n = 0; n < divs.length; n++) {
+    //   lastArr[lastArr.length] = divs[n];
+    // }
   }
   return arrRes;
 }
+
+// divisors(arr[0])[2];
 
 console.log(primeSort([2,4,9,68,10,144,100]));
